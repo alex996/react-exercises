b6bbdca042543520439d169502370fc2613f8510
Alex N <alexnezhynsky@gmail.com>
2020-08-19 00:33:33 -0600

    Bump deps

--- webpack.config.js
+++ webpack.config.js
@@ -10,11 +10,6 @@ module.exports = {
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
     ]
   },
-  resolve: {
-    alias: {
-      '@material-ui/core': '@material-ui/core/es'
-    }
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html'
